1f1dfbd3b27db55d66fc84791710c5bd5f117ccc
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2022-02-21 01:12:31 -0300

    Add Dracula GTK theme setup script

---
+++ scripts/setup-dracula-gtk.sh
@@ -0,0 +1,6 @@
+wget https://github.com/dracula/gtk/archive/master.zip -O ~/.dracula-theme/gtk.zip
+
+unzip ~/.dracula-theme/gtk.zip -d ~/themes
+
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"

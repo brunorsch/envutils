f65b495718b6caa320ad0dbf5a43872f259b3ee5
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2022-06-20 02:19:10 -0300

    Add setup-ngrok script

---
+++ scripts/setup-ngrok.sh
@@ -0,0 +1,8 @@
+# Download files (Tarball)
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+
+# Decompress and put the downloaded binaries into /bin
+sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+
+# Delete the downloaded tarball
+rm ngrok-v3-stable-linux-amd64.tgz

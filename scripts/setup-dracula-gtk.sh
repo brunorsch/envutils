caac689f821d3143717767cdae1300f281e146de
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2022-06-27 23:16:24 -0300

    Fix: Create .themes

--- scripts/setup-dracula-gtk.sh
+++ scripts/setup-dracula-gtk.sh
@@ -1,8 +1,8 @@
-mkdir ~/.dracula-theme
+mkdir ~/.dracula-theme ~/.themes
 
 wget https://github.com/dracula/gtk/archive/master.zip -O ~/.dracula-theme/gtk.zip
 
-unzip ~/.dracula-theme/gtk.zip -d ~/themes
+unzip ~/.dracula-theme/gtk.zip -d ~/.themes
 
 gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
 gsettings set org.gnome.desktop.wm.preferences theme "Dracula"

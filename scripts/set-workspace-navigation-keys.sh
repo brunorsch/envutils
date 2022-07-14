afb6247a90d1ac2dd99339082e9c08bc839d863f
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2022-07-13 23:06:39 -0300

    Add workspace keys config script

---
+++ scripts/set-workspace-navigation-keys.sh
@@ -0,0 +1,9 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Shift><Alt>w']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Shift><Alt>s']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Shift><Alt>d']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Shift><Alt>a']"
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Control><Alt>w']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Control><Alt>s']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Control><Alt>d']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Control><Alt>a']"
